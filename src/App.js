067840c71d1d5e9e6d5406971e72e314cb75f3f6
KawshikBss <kawshikbss@gmail.com>
2022-06-28 05:51:35 +0600

    made some minor modifications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from "./Visualizer/Grid/Grid";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid rows={20} cols={50} />
     </div>
   );
 }
