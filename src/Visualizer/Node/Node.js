a992d87c4347e3492530f5007cabbea199f9f5b3
KawshikBss <kawshikbss@gmail.com>
2022-06-28 13:10:16 +0600

    modeified datastructure and integrated it with state cycle for ui

--- src/Visualizer/Node/Node.js
+++ src/Visualizer/Node/Node.js
@@ -1,7 +1,13 @@
 import "./Node.css";
 
 function Node({ status, handleClick }) {
-  return <div className={`node ${status}`} onClick={handleClick}></div>;
+  return (
+    <div
+      className={`node ${status}`}
+      onClick={handleClick}
+      onDrag={handleClick}
+    ></div>
+  );
 }
 
 export default Node;
