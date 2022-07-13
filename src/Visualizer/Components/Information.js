49346d4e20ee45ba08f8ae61fb98e71179fca9c2
KawshikBss <kawshikbss@gmail.com>
2022-07-13 14:06:06 +0600

    added information tab

---
+++ src/Visualizer/Components/Information.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Node from "../Node/Node";
+import "../../styles/Information.css";
+
+function Information() {
+  return (
+    <div>
+      <span className="info-tab">
+        <span className="info-tab-item">
+          <Node status={"start"} />
+          <h3>Start Node</h3>
+        </span>
+        <span className="info-tab-item">
+          <Node status={"end"} />
+          <h3>End Node</h3>
+        </span>
+        <span className="info-tab-item">
+          <Node status={"visited"} />
+          <h3>Visited Node</h3>
+        </span>
+        <span className="info-tab-item">
+          <Node status={"path"} />
+          <h3>Path Node</h3>
+        </span>
+        <span className="info-tab-item">
+          <Node status={"Wall"} />
+          <h3>Wall Node</h3>
+        </span>
+      </span>
+    </div>
+  );
+}
+
+export default Information;
